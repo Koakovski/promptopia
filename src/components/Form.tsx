a22ddefd70217cfa70d736ee496e39137718a56c
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-06-05 10:22:18 -0300

    feat: add Form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,92 @@
+import { PostModel } from "@models";
+import Link from "next/link";
+import { ChangeEvent, Dispatch, FC, FormEvent, SetStateAction } from "react";
+
+interface FormProps {
+    type: "Create";
+    post: PostModel;
+    setPost: Dispatch<SetStateAction<PostModel>>;
+    isSubmiting: boolean;
+    handleSubmit: (e: FormEvent<HTMLFormElement>) => void;
+}
+
+export const Form: FC<FormProps> = ({
+    type,
+    post,
+    setPost,
+    isSubmiting,
+    handleSubmit,
+}) => {
+    const setPrompt = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        setPost((prevState) => ({
+            ...prevState,
+            prompt: event.currentTarget.value,
+        }));
+    };
+
+    const setTag = (event: ChangeEvent<HTMLInputElement>) => {
+        setPost((prevState) => ({
+            ...prevState,
+            tag: event.currentTarget.value,
+        }));
+    };
+
+    return (
+        <section className="w-full max-w-full flex-start flex-col">
+            <h1 className="head_text tex-left">
+                <span className="blue_gradient">{type} Post</span>
+            </h1>
+            <p className="desc text-left max-w-md">
+                {type} and share amazing prompts with the world, and let your imagination
+                run wild with any AI-powered platform.
+            </p>
+            <form
+                onSubmit={handleSubmit}
+                className="mt-10 w-full mb-5 max-wl-2xl flex flex-col gap-7 glassmorphism"
+            >
+                <label>
+                    <span className="font-satoshi font-semibold text-base text-gray-700">
+                        Your AI prompt
+                    </span>
+                    <textarea
+                        value={post.prompt}
+                        onChange={setPrompt}
+                        placeholder="Write your prompt here..."
+                        required
+                        className="form_textarea"
+                    />
+                </label>
+
+                <label>
+                    <span className="font-satoshi font-semibold text-base text-gray-700">
+                        Tag{" "}
+                        <span className="font-normal">
+                            (#product, #webdevelopment, #idea)
+                        </span>
+                    </span>
+                    <input
+                        value={post.tag}
+                        onChange={setTag}
+                        placeholder="#tag"
+                        required
+                        className="form_input"
+                    />
+                </label>
+
+                <div className="flex-end mx-3 mb-5 gap-4">
+                    <Link href="/" className="text-gray-500 text-sm">
+                        Cancel
+                    </Link>
+
+                    <button
+                        type="submit"
+                        disabled={isSubmiting}
+                        className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
+                    >
+                        {isSubmiting ? `${type}...` : `${type}`}
+                    </button>
+                </div>
+            </form>
+        </section>
+    );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./Feed";
 export * from "./Nav";
+export * from "./Form";
 export * from "./SessionProvider";
