db0746a58b453beedba1398bc8dd9fa377404552
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-06-05 10:22:30 -0300

    feat: add create prompt page

---
+++ src/app/create-prompt/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Form } from "@components";
+import { PostModel } from "@models";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+
+const CreatePromptPage = () => {
+    const router = useRouter();
+
+    const [isSubmting, setIsSubmeting] = useState(false);
+    const [post, setPost] = useState<PostModel>({ id: "", prompt: "", tag: "" });
+    const { data: session } = useSession();
+
+    const createPrompt = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setIsSubmeting(true);
+
+        try {
+            const response = await fetch("/api/prompt/new", {
+                method: "POST",
+                body: JSON.stringify({
+                    userId: session?.user?.id,
+                    prompt: post.prompt,
+                    tag: post.tag,
+                }),
+            });
+
+            if (response.ok) {
+                router.push("/");
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsSubmeting(false);
+        }
+    };
+
+    return (
+        <Form
+            type="Create"
+            post={post}
+            setPost={setPost}
+            isSubmiting={isSubmting}
+            handleSubmit={createPrompt}
+        />
+    );
+};
+
+export default CreatePromptPage;
